376882aaf740419681568ac6da0cf10dc5a5c81d
Ovidiu Ciule <ovidiu.ciule@gmail.com>
2009-02-24 01:10:20 +0100

    first commit

---
+++ band.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from google.appengine.api import urlfetch
+from google.appengine.ext import db
+
+
+class Band(db.Model):
+  """Band model
+  """
+  created = db.DateTimeProperty(auto_now_add=True)
+  debut_album_cover_path = db.StringProperty()
+  debut_album_cover_link = db.StringProperty()
+  name = db.StringProperty()
+  debut_album_name = db.StringProperty()
+
+  def randomize(self):
+    """Generate random band.
+       Band name : Wikipedia Special:Random
+       Debut album name : Wikiquote Special:Random
+       Debut album cover : Flickr, "last seven day page", choose the 3rd pic
+    """
+    self.name, self.content = Band.getRandomWikipediaPageTitle()
+    self.debut_album_name, content = Band.getRandomWikiquotePageTitle()
+    self.debut_album_cover_path, self.debut_album_cover_link = Band.getRandomFlickrPhotoPath()
+    
+  @staticmethod
+  def getRandomWikiMediaPageTitle(
+    random_url = "http://en.wikipedia.org/wiki/Special:Random",
+    end_title_token = "- Wikipedia, the free",
+    ):
+    retries = 0
+    max_retries = 5
+    while retries < max_retries:
+      random_page = urlfetch.fetch(random_url, payload=None, method=urlfetch.GET, headers={}, allow_truncated=True, follow_redirects=True)
+      content = unicode(random_page.content.decode("latin-1"))
+      ix_title_start = content.find("<title>")
+      ix_title_end = content.find("</title>")
+    
+      if ix_title_start > -1 and ix_title_end > -1 and ix_title_start < ix_title_end:
+        title = content[ix_title_start+7:ix_title_end]
+        ix_title_end = title.find(end_title_token)
+        if ix_title_end > -1:
+          title = title[0:ix_title_end]
+          return title, content
+      retries += 1
+      
+  @staticmethod
+  def getRandomWikipediaPageTitle():
+    return Band.getRandomWikiMediaPageTitle(
+      random_url = "http://en.wikipedia.org/wiki/Special:Random",
+        end_title_token = "- Wikipedia, the free")
+
+  @staticmethod
+  def getRandomWikiquotePageTitle():
+    return Band.getRandomWikiMediaPageTitle(random_url = "http://en.wikiquote.org/wiki/Special:Random",
+      end_title_token = "- Wikiquote")
+
+  @staticmethod
+  def getRandomFlickrPhotoPath():
+    random_url = "http://www.flickr.com/explore/interesting/7days/"
+    first_image_start = "\"http://farm"
+    first_image_end   = ".jpg\" "
+
+    first_link_start  = "href=\"/photos/"
+    first_link_end    = "\" "
+
+    retries = 0
+    max_retries = 5
+    while retries < max_retries:
+      random_page = urlfetch.fetch(random_url, payload=None, method=urlfetch.GET, headers={}, allow_truncated=True, follow_redirects=True)
+      content = unicode(random_page.content.decode("latin_1"))
+      
+      ix_first_image_start = content.find(first_image_start)
+
+      if ix_first_image_start > -1:        
+        path = content[ix_first_image_start+11:]
+
+        ix_first_link_start = path.find(first_link_start)
+        if ix_first_link_start > -1:
+
+          link = path[ix_first_link_start+6:]
+          ix_first_link_end = link.find(first_link_end)
+          if ix_first_link_end > -1:
+            link = link[0:ix_first_link_end]
+
+            ix_first_image_start = path.find(first_image_start)
+            if ix_first_image_start > -1:
+              path = path[ix_first_image_start+1:]
+              ix_title_end = path.find(first_image_end)
+              path = path[0:ix_title_end+4]
+              return path, link
+      retries += 1
+      
+  def fix_encoding(self):
+    self.name = self.name.encode('latin-1')
+    self.debut_album_name = self.debut_album_name.encode('latin-1')
---
+++ main.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+from band import Band
+import os
+import wsgiref.handlers
+
+
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+from google.appengine.ext import db
+
+
+# Set the debug level
+_DEBUG = True
+
+
+class BaseRequestHandler(webapp.RequestHandler):
+  """Base request handler extends webapp.Request handler
+
+     It defines the generate method, which renders a Django template
+     in response to a web request
+  """
+
+  def generate(self, template_name, template_values={}):
+    """Generate takes renders and HTML template along with values
+       passed to that template
+
+       Args:
+         template_name: A string that represents the name of the HTML template
+         template_values: A dictionary that associates objects with a string
+           assigned to that object to call in the HTML template.  The defualt
+           is an empty dictionary.
+    """
+
+    latest_bands = db.GqlQuery("SELECT * FROM Band ORDER BY created DESC LIMIT 5")
+    #latest_bands = map(lambda x: x.fix_encoding(), latest_bands)
+    latest_fixed = []
+    for band in latest_bands:
+        band.fix_encoding()
+        latest_fixed.append(band)
+    values = {'latest_bands': latest_fixed}
+    template_values.update(values)
+    
+    # Construct the path to the template
+    directory = os.path.dirname(__file__)
+    path = os.path.join(directory, 'templates', template_name)
+
+    # Respond to the request by rendering the template
+    self.response.out.write(template.render(path, template_values, debug=_DEBUG))
+
+class MainHandler(BaseRequestHandler):
+
+  def get(self):
+    band = Band()
+    band.randomize()
+    band.put()
+    band.fix_encoding()
+    self.generate('index.html', template_values={'page_title': 'Band Generator - Generate a band!',
+                                                'band' : band,
+                                                })
+
+class ViewHandler(BaseRequestHandler):
+
+  def get(self, band_id):
+    try:
+      band_id = int(band_id)
+    except ValueError:
+      self.generate('index.html', template_values={'page_title': '404 - Band Generator - Generate a band!',
+                                                  'band' : {'name': '404'},
+                                                  })
+      return
+
+    band = Band.get_by_id(band_id)
+    band.fix_encoding()
+    self.generate('index.html', template_values={'page_title': 'Band Generator - Generate a band!',
+                                                'band' : band,
+                                                })
+
+_URLS = [('/',              MainHandler),
+         ('/band/',         MainHandler),
+         ('/band/([^/]+)',  ViewHandler)]
+
+def main():
+  application = webapp.WSGIApplication(_URLS,
+                                       debug=_DEBUG)
+  wsgiref.handlers.CGIHandler().run(application)
+
+
+if __name__ == '__main__':
+  main()
